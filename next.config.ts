4a44050812b9344bfd3538b46dbc16bf6fa0d909
Gokuattacker <abhigyangoswami0@gmail.com>
2026-08-23 12:34:41 +0530

    comment out PDF header configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/Abhigyan_Goswami.pdf",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "application/pdf",
-          },
-          {
-            key: "Content-Disposition",
-            value: 'inline; filename="Abhigyan_Goswami.pdf"',
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/Abhigyan_Goswami.pdf",
+  //       headers: [
+  //         {
+  //           key: "Content-Type",
+  //           value: "application/pdf",
+  //         },
+  //         {
+  //           key: "Content-Disposition",
+  //           value: 'inline; filename="Abhigyan_Goswami.pdf"',
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
